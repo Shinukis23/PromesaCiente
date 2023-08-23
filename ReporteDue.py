c72668c1cac15bd4019a818cd34ae1c6ed6b2884
Shinukis23 <juan.a.o.delgado@gmail.com>
2023-08-23 09:59:08 -0700

    Se modifico funcion agrupar para agregar duedate vendedor

--- ReporteDue.py
+++ ReporteDue.py
@@ -56,18 +56,113 @@ datos.info()
 indexDeleted = datos[(datos['Job Status'].isin(['Voided', 'New'])) | (datos['Diferencia DueDates'].isnull()) | (datos['Part Store #'].isin([20,21]))].index
 datos.drop(indexDeleted,inplace=True) 
 datos.to_excel(r'Revision.xlsx')
-#indexDeleted = datos[datos['Drop Location'] ==  'ACOMODOWC1'].index
-#  merged_data.drop(indexDeleted,inplace=True)
-
-#vendedores = datos.groupby(['Created by (Salesperson)','Diferencia DueDates']).count()
-
-# Groupby & multiple aggregations
-#result1 = datos.groupby(['Created by (Salesperson)', 'Diferencia DueDates']).size().reset_index(name='Count')
 
 datos['Menor que 0'] = datos['Diferencia DueDates'] < 0
+"""def crear_lista3(group):
+	#return list(zip(group['Created'], group['Menor que 0'],group['Due_Date_Vendedor']))
+	count_true = group['Menor que 0'].sum()  # Cuenta cuántos son True
+	count_false = len(group) - count_true    # Cuenta cuántos son False
+	#return [(count_true, count_false)]
+	return [(list(zip(group['Due_Date_Vendedor'], group['Menor que 0'])),count_true, count_false)]
+# Agrupa los datos por 'Salesperson', 'Customer' y 'Created' con una frecuencia de 1 minuto
+result3 = datos.groupby([
+    'Created by (Salesperson)',
+    'Customer',
+    pd.Grouper(key='Created', freq='1T'),
+    pd.Grouper(key='Due_Date_Calculado') # Agrupa por hora con diferencia de 1 minuto
+]).apply(crear_lista3)
 
-# Agrupar por 'Created by (Salesperson)' y la nueva columna 'Menor que 0'
-result = datos.groupby(['Created by (Salesperson)', 'Menor que 0']).size().reset_index(name='Count')
+result3.to_excel(r'DueDate_Grupo7.xlsx')
+"""
+#Perfecto
+def crear_lista(group):
+    count_true = group['Menor que 0'].sum()  # Cuenta cuántos son True
+    count_false = len(group) - count_true    # Cuenta cuántos son False
+
+    due_dates = group['Due_Date_Vendedor'].tolist()
+    
+    return [(count_true, count_false, due_dates)]
+
+    #return [(count_true, count_false)]
+
+result = datos.groupby([
+    'Created by (Salesperson)',
+    'Customer',
+    pd.Grouper(key='Created', freq='1T'),
+    pd.Grouper(key='Due_Date_Calculado')
+]).apply(crear_lista)
+
+result.to_excel(r'DueDate_Grupo4.xlsx')
+
+#############
+def crear_lista2(group):
+    count_true = group['Menor que 0'].sum()  # Cuenta cuántos son True
+    count_false = len(group) - count_true    # Cuenta cuántos son False
+    return [(count_true, count_false)]
+
+result2 = datos.groupby([
+    'Created by (Salesperson)',
+    'Customer',
+    pd.Grouper(key='Created', freq='1T'),
+    pd.Grouper(key='Due_Date_Calculado')
+]).apply(crear_lista2)
+
+
+# Agregar filas con la cantidad de True y False por Salesperson
+salesperson_counts = datos.groupby('Created by (Salesperson)')['Menor que 0'].value_counts().unstack(fill_value=0)
+salesperson_counts.reset_index(inplace=True)
+salesperson_counts.rename(columns={True: 'True Salesperson', False: 'False Salesperson'}, inplace=True)
+
+# Convertir la Serie 'result' en un DataFrame
+result2 = result2.to_frame(name='Counts')
+
+# Fusionar el DataFrame 'result' con 'salesperson_counts'
+result2 = result2.merge(salesperson_counts, how='left', left_on='Created by (Salesperson)', right_on='Created by (Salesperson)')
+
+
+result2.to_excel(r'DueDate_Grupo6.xlsx')
+#result1.to_excel(r'porgruponew1.xlsx')
+
+#""" Este tambien esta bien
+def crear_lista3(group):
+    count_true = group['Menor que 0'].sum()  # Cuenta cuántos son True
+    count_false = len(group) - count_true    # Cuenta cuántos son False
+    
+    rows = [
+        (group.name[0], group.name[1], group.name[2], count_true, True),
+        (group.name[0], group.name[1], group.name[2], count_false, False)
+    ]
+    
+    return pd.DataFrame(rows, columns=['Created by (Salesperson)', 'Customer', 'Created', 'Count', 'Menor que 0'])
+
+# Agrupa los datos por 'Salesperson', 'Customer' y 'Created' con una frecuencia de 1 minuto
+result3 = datos.groupby([
+    'Created by (Salesperson)',
+    'Customer',
+    pd.Grouper(key='Created', freq='1T'),
+    pd.Grouper(key='Due_Date_Calculado')  # Agrupa por hora con diferencia de 1 minuto
+]).apply(crear_lista3)
+
+# Resetear el índice del DataFrame
+result3.reset_index(drop=True, inplace=True)
+
+# Filtrar las filas con True
+true_rows = result3[result3['Menor que 0']]
+false_rows = result3[result3['Menor que 0']==0]
+
+# Calcular la cantidad de True por Salesperson
+salesperson_counts = true_rows.groupby('Created by (Salesperson)')['Count'].sum().reset_index()
+salesperson_countsfalse = false_rows.groupby('Created by (Salesperson)')['Count'].sum().reset_index()
+# Agregar las filas de True Count al DataFrame original
+result3 = pd.concat([result3, salesperson_counts,salesperson_countsfalse], ignore_index=True, sort=False)
+
+# Ordenar el DataFrame por Salesperson y Created
+result3.sort_values(by=['Created by (Salesperson)', 'Created'], inplace=True)
+
+# Guardar el DataFrame en un archivo Excel
+result3.to_excel(r'DueDate_Grupo8.xlsx', index=False)
+
+resultor = datos.groupby(['Created by (Salesperson)', 'Menor que 0']).size().reset_index(name='Count')
 
 
 #print(result)
@@ -79,9 +174,7 @@ result = datos.groupby(['Created by (Salesperson)', 'Menor que 0']).size().reset
 # Guardar los cambios en el archivo de Excel
 ####writer.save()
 ####writer.close()
-result.to_excel(r'DueDate_Grupo.xlsx')
-#result1.to_excel(r'porgruponew1.xlsx')
-
+resultor.to_excel(r'DueDate_Grupo.xlsx')
 
 print(vendedores)
 #vendedores.groups()
